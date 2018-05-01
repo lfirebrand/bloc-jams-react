da5cc4ab44174aa2aba402867930a0c03e72f6e1
Lexi Zgud <lexibitesblog@gmail.com>
2018-04-30 21:51:18 -0500

    Checkpoint Jams Landing: Completed w/ error fixed

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ const Landing = () => (
   <section className="selling-points">
     <div className="point">
       <h2 className="point-title">Choose your music</h2>
-      <p> className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+      <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
       </div>
     <div className="point">
       <h2 className="point-title">Unlimited, streaming, ad-free</h2>
