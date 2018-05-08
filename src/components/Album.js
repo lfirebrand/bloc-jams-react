a22d3f6d9504987c03f572753d760b4968b35e46
Lexi Zgud <lexibitesblog@gmail.com>
2018-05-07 21:05:47 -0500

    Checkpoint Bloc Jams Audio Playback: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,10 +10,39 @@ class Album extends Component {
     });
 
     this.state = {
-        album: album
+        album: album,
+        currentSong: album.songs[0],
+        isPlaying: false
     };
-  }
 
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+    }
+
+    play() {
+      this.audioElement.play();
+      this.setState({ isPlaying: true });
+    }
+
+    pause() {
+      this.audioElement.pause();
+      this.setState({ isPlaying: false });
+    }
+
+    setSong(song) {
+      this.audioElement.src = song.audioSrc;
+      this.setState({ currentSong: song });
+    }
+
+    handleSongClick(song) {
+      const isSameSong = this.state.currentSong === song;
+      if (this.state.isPlaying && isSameSong) {
+        this.pause();
+      } else {
+        if (!isSameSong) { this.setSong(song); }
+        this.play();
+      }
+    }
 
   render() {
     return (
@@ -36,11 +65,11 @@ class Album extends Component {
           <tbody>
             {
               this.state.album.songs.map( (song, index) =>
-              <tr className="song" key={index}>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                 <td className="song-number">{index + 1}
                   </td>
                 <td className="song-title">{song.title}</td>
-                <td className="song-duration">{(song.duration / 60).toFixed(2) + " mins"}</td>
+                <td className="song-duration">{(song.duration)}</td>
                 <td>
                 <span className="ion-play"></span>
                 <span className="ion-pause"></span>
